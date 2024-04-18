e03adbb6459c997b2bfc463b1556e48ffb889851
Mosi <mosisu07@gmail.com>
2024-04-18 17:22:17 +0800

    feat: add theme provider.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App.jsx";
 import GlobalStyle from "./constants/globalStyle.js";
+import theme from "./constants/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
